24caba8a0ef4dd7e2b1bc28f880cc7a7c9aa2712
Brandon Martin <brandon.martin@bcgdv.com>
2019-10-23 15:09:56 -0700

    Troubleshooting

--- scripts/dependencies_sha.sh
+++ scripts/dependencies_sha.sh
@@ -2,7 +2,7 @@
 set -e
 
 eval "$(jq -r '@sh "NAME=\(.name) DEPENDENCIES_FILE=\(.modules_file)"')"
-
+echo "From dependencies: $DEPENDENCIES_FILE"
 if [ "$DEPENDENCIES_FILE" != "null" ]; then
   current_sha=$(shasum -a 256 "${DEPENDENCIES_FILE}" | cut -d " " -f 1)
   
