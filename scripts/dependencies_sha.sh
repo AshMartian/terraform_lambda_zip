da595b887b48e2f248de27a9eb1aa5885588c244
Brandon Martin <brandon.martin@bcgdv.com>
2019-10-23 15:20:41 -0700

    Troubleshooting

--- scripts/dependencies_sha.sh
+++ scripts/dependencies_sha.sh
@@ -10,4 +10,4 @@ else
   current_sha=$(echo "$NAME" | shasum -a 256 - | cut -d " " -f 1)
 fi
 
-jq -n --arg dep_sha "$current_sha" '{"sha":"$dep_sha"}'
+jq -n --arg dep_sha "$current_sha" '{"sha": $dep_sha}'
